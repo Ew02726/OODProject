73278195245ec08eb93b27da48fbe0183fe5f88b
chibubella <31222947+chibubella@users.noreply.github.com>
2019-11-07 14:34:11 -0500

    Delete TAG_Handicap.java

--- TAG_Handicap.java
+++
@@ -1,9 +0,0 @@
-
-public class TAG_Handicap implements ParkingSpotTag{
-	TagType tagType = TagType.CONSTRAINT;
-	String description = "Handicap Spot";
-
-	public String getDescription() {
-		return description;
-	}
-}
