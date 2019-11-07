9ebf7a741130a889845492d67bfb9a27dc730353
chibubella <31222947+chibubella@users.noreply.github.com>
2019-11-07 14:38:06 -0500

    Delete TAG_NearLandrum.java

--- TAG_NearLandrum.java
+++
@@ -1,9 +0,0 @@
-
-public class TAG_NearLandrum implements ParkingSpotTag{
-	TagType tagType = TagType.PROXIMITY;
-	String description = "Near Main Dining Commons";
-
-	public String getDescription() {
-		return description;
-	}
-}
