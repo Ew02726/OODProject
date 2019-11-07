8e511b2047e028b04db8420110915b083600277f
chibubella <31222947+chibubella@users.noreply.github.com>
2019-11-07 14:37:58 -0500

    Delete TAG_NearIT.java

--- TAG_NearIT.java
+++
@@ -1,9 +0,0 @@
-
-public class TAG_NearIT implements ParkingSpotTag{
-	TagType tagType = TagType.PROXIMITY;
-	String description = "Near IT Building";
-
-	public String getDescription() {
-		return description;
-	}
-}
